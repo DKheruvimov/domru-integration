f7160f2f9212af89413111d0a63c93d66421e4bf
DKheruvimov <rusillusion@gmail.com>
2026-07-08 14:43:39 +0300

    chore: clean up garbage files, directories, and unused dependencies

--- test.js
+++
@@ -1,6 +0,0 @@
-const { isScheduleActive } = require('./dist/server.cjs');
-const fs = require('fs');
-const people = JSON.parse(fs.readFileSync('/app/data/people.json', 'utf8'));
-const activeGuest = people.find(p => p.role !== 'resident' && isScheduleActive(p));
-console.log(activeGuest ? 'TRUE' : 'FALSE');
-if (activeGuest) console.log(activeGuest.name);
--- test4.cjs
+++
@@ -1,7 +0,0 @@
-const { isScheduleActive } = require('./dist/server.cjs');
-const fs = require('fs');
-const people = JSON.parse(fs.readFileSync('./data/people.json', 'utf8'));
-people.forEach(p => {
-  const active = isScheduleActive(p);
-  console.log(`${p.name} | enabled: ${p.enabled} | active: ${active}`);
-});
